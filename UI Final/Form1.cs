eddc23443a24f2fb66a44887684570bf1bb28614
Caleb Perez <caleb.perez@oit.edu>
2023-06-12 10:38:28 -0700

    Final finished

--- UI Final/Form1.cs
+++ UI Final/Form1.cs
@@ -1,7 +1,7 @@
-namespace UI_Final
-using System;
-using System.Data.SqlClient;
-{
+namespace UI_Final 
+
+{   using System;
+    using System.Data.SqlClient;
     public partial class Form1 : Form
     {
         private DataSender dataSender;
@@ -14,24 +14,28 @@ using System.Data.SqlClient;
         {
         string data = txtAboutUs.Text;
         dataSender.InsertData(data);
+        //This would take you to the About us Page
     }
 
         private void btnAC_Click(object sender, EventArgs e)
         {
         string data = txtAC.Text;
         dataSender.InsertData(data);
+        //This would send you to Avian consulting page
     }
 
         private void btnOurProducts_Click(object sender, EventArgs e)
         {
         string data = txtOurProducts.Text;
         dataSender.InsertData(data);
+        //This would send you to the Our products page
     }
 
         private void btnFI_Click(object sender, EventArgs e)
         {
         string data = txtFI.Text;
         dataSender.InsertData(data);
+        //This would send you to the For Investors page
     }
     }
 }
