5e0c73be1fdf133072ecadd480d2eef6a7df5402
Caleb Perez <caleb.perez@oit.edu>
2023-06-11 19:57:32 -0700

    Update to code sucess

--- UI Final/Form1.cs
+++ UI Final/Form1.cs
@@ -1,7 +1,10 @@
 namespace UI_Final
+using System;
+using System.Data.SqlClient;
 {
     public partial class Form1 : Form
     {
+        private DataSender dataSender;
         public Form1()
         {
             InitializeComponent();
@@ -9,22 +12,26 @@ namespace UI_Final
 
         private void btnAboutUs_Click(object sender, EventArgs e)
         {
-
-        }
+        string data = txtAboutUs.Text;
+        dataSender.InsertData(data);
+    }
 
         private void btnAC_Click(object sender, EventArgs e)
         {
-
-        }
+        string data = txtAC.Text;
+        dataSender.InsertData(data);
+    }
 
         private void btnOurProducts_Click(object sender, EventArgs e)
         {
-
-        }
+        string data = txtOurProducts.Text;
+        dataSender.InsertData(data);
+    }
 
         private void btnFI_Click(object sender, EventArgs e)
         {
-
-        }
+        string data = txtFI.Text;
+        dataSender.InsertData(data);
+    }
     }
 }
